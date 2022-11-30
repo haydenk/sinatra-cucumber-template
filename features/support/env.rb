9db7d7b73cdff331e567c46721d28d40aa042ba0
Hayden <haydenk@users.noreply.github.com>
2022-11-29 22:53:02 -0600

    make cucumber run headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -22,7 +22,7 @@ class SinatraWorld
   # Download the ChromeDriver from: https://code.google.com/p/selenium/wiki/ChromeDriver
   #
   Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+    Capybara::Selenium::Driver.new(app, :browser => :chrome, options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless no-sandbox disable-gpu]))
   end
 
   # use the rackup file to load the apps w/their respective URL mappings, sweet!
